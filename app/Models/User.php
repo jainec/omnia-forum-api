ea534b2607f0068e86bea4dbee738acc05ca0cfb
Jaine da Conceição Santos <jayne-conceicao@hotmail.com>
2020-04-27 02:00:41 -0300

    add sign up

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,8 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims() {
         return [];
     }
+
+    public function setPasswordAttribute($value) {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
